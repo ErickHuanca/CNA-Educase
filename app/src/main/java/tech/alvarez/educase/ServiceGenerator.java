4125102cb16de55a95ca347328c1440e0f8834e7
Daniel Alvarez <a3dany@gmail.com>
2017-09-05 16:09:59 -0400

    Clean ServiceGenerator

--- app/src/main/java/tech/alvarez/educase/ServiceGenerator.java
+++ app/src/main/java/tech/alvarez/educase/ServiceGenerator.java
@@ -1,10 +1,7 @@
 package tech.alvarez.educase;
 
-import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
-
-//import okhttp3.logging.HttpLoggingInterceptor;
 
 public class ServiceGenerator {
 
@@ -17,18 +14,7 @@ public class ServiceGenerator {
 
     private static Retrofit retrofit = builder.build();
 
-    private static OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-//    private static HttpLoggingInterceptor logging = new HttpLoggingInterceptor()
-//            .setLevel(HttpLoggingInterceptor.Level.BODY);
-
-
     public static <S> S createService(Class<S> serviceClass) {
-//        if (!httpClient.interceptors().contains(logging)) {
-//            httpClient.addInterceptor(logging);
-//            builder.client(httpClient.build());
-//            retrofit = builder.build();
-//        }
         return retrofit.create(serviceClass);
     }
 
